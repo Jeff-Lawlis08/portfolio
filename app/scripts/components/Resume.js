78d75e5b13b143df1d54889239d887e8357b0d64
Jeff Lawlis <jeff.lawlis@yahoo.com>
2017-01-12 14:46:09 -0600

    edited resume

--- app/scripts/components/Resume.js
+++ app/scripts/components/Resume.js
@@ -4,7 +4,7 @@ export default React.createClass({
   render(){
     return (
       <figure className="resume-figure">
-        <img src="../../assets/images/Jeff_Lawlis_Resume.jpg"/>
+        <img src="../../assets/images/Jeff-Lawlis-Resume.jpg"/>
       </figure>
     )
   }
