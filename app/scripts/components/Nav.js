7bb63536c84cc15055130a6d74b6c22c14a6e49a
Jeffrey Lawlis <jeff.lawlis@yahoo.com>
2017-03-02 16:44:52 -0600

    Update Nav.js

--- app/scripts/components/Nav.js
+++ app/scripts/components/Nav.js
@@ -2,19 +2,51 @@ import {Link} from 'react-router';
 import React from 'react';
 
 export default React.createClass({
+  getInitialState(){
+    return {
+      dropdown: false
+    }
+  },
   render(){
+    let menu;
+    if(this.state.dropdown===true){
+      menu = (
+          <div id='dropdown'>
+            <a onClick={this.handleClick} href="/#home">HOME</a>
+            <Link onClick={this.handleClick} to="/resume">RÈSUMÈ</Link>
+            <a onClick={this.handleClick} href="/#project-container">PROJECTS</a>
+            <a onClick={this.handleClick} href="/#personal">ABOUT ME</a>
+            <a onClick={this.handleClick} href="/#form-container">CONTACT</a>
+          </div>
+        )
+    } else {
+      menu = (<span/>)
+    }
     return(
       <nav>
         <div id='name-title'>
           <span>Jeff Lawlis</span>
           <span>Web Developer</span>
         </div>
-        <a href="/#form-container">CONTACT</a>
-        <a href="/#personal">ABOUT ME</a>
-        <a href="/#project-container">PROJECTS</a>
-        <Link to="/resume">RÈSUMÈ</Link>
-        <a href="/#home">HOME</a>
+        <div id='links'>
+          <a href="/#home">HOME</a>
+          <Link to="/resume">RÈSUMÈ</Link>
+          <a href="/#project-container">PROJECTS</a>
+          <a href="/#personal">ABOUT ME</a>
+          <a href="/#form-container">CONTACT</a>
+        </div>
+        <div id='hamburger'>
+          <span onClick={this.handleClick}><i className="fa fa-bars" aria-hidden="true"></i></span>
+          {menu}
+        </div>
       </nav>
     );
+  },
+  handleClick(e){
+    if(this.state.dropdown===false){
+      this.setState({dropdown: true});
+    } else {
+      this.setState({dropdown: false});
+    }
   }
 });
