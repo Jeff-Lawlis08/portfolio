884491711ba3fd7706c77bd77fd60fd7d2cca110
Jeffrey Lawlis <jeff.lawlis@yahoo.com>
2019-02-14 17:01:46 -0600

    Update Personal.js

--- app/scripts/components/Personal.js
+++ app/scripts/components/Personal.js
@@ -7,7 +7,7 @@ export default React.createClass({
       <div id="personal">
         <h2>About Me</h2>
         <img src="../../assets/images/headshot-min.jpg"/>
-        <p>I am a Junior Front-End Developer who just completed a 12 week immersive course at The Iron Yard. I've always been interested in technology and how things work. I started off as a Computer Science major before deciding to attend The Iron Yard. I'm thrilled to begin a career in something I love to do, and continuing to learn and grow as a developer and as a person.
+        <p>I am a Junior Developer in the process of completing the Coding Boot Camp at the University of Texas. I've always been interested in technology and how things work. I started off as a Computer Science major before deciding to attend the Boot Camp. I am thrilled to begin a career in something I love to do, and continuing to learn and grow as a developer and as a person.
         </p>
       </div>
     );
